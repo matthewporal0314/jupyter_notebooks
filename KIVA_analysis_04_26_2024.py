d1221dc92ae9e8efc2c31f8e3f155ceade49a7ca
matthewporal0314 <168119970+matthewporal0314@users.noreply.github.com>
2024-04-26 17:59:48 +0800

    Update KIVA_analysis_04_26_2024.py

--- KIVA_analysis_04_26_2024.py
+++ KIVA_analysis_04_26_2024.py
@@ -1,56 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 50000)
 
-
-# In[2]:
-
-
 data = pd.read_csv('kiva_loans.csv')
 
-
-# In[4]:
-
-
 data.head()
-
-
-# In[5]:
-
 
 data['posted_time'] = pd.to_datetime(data['posted_time'])
 data['disbursed_time'] = pd.to_datetime(data['disbursed_time'])
 data['funded_time'] = pd.to_datetime(data['funded_time'])
 
-
-# In[10]:
-
-
 data['loan_crowdsourcing_time'] = (data.funded_time-data.posted_time).dt.days
 data['date'] = pd.to_datetime(data['date'])
 data['month'] = data['date'].apply(lambda x: x.strftime('%Y-%m-01'))
 
-
-# In[11]:
-
-
 data.head()
 
-
-# In[12]:
-
-
 data.groupby(["month"])["loan_crowdsourcing_time"].mean().reset_index()
-
-
-# In[13]:
 
 
 import matplotlib.pyplot as plt
@@ -64,3 +35,7 @@ fig, ax = plt.subplots(figsize=(20, 7.5))
         .plot.bar(ax=ax)
 )
 
+a = data.groupby(["country"])["loan_crowdsourcing_time"].mean().reset_index().sort_values(by="loan_crowdsourcing_time").sort_values(by='loan_crowdsourcing_time', ascending=False).reset_index(drop=True).head(10)
+a["loan_crowdsourcing_time"] = a["loan_crowdsourcing_time"].round(2)
+a.columns = ['country','loan_crowdsourcing_time (days)']
+a
